8e683682e4bc4aa47658739a0cb55a1e14c246db
John Michael <byjohnmichael@gmail.com>
2025-09-12 18:54:11 -0600

    Coming soong page added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="coming-soon">
+        <h1>byjohnmichael.com coming soon...</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
