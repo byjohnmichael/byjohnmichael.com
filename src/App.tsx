f3ad3835d7dbc1fc3147c2fe49bc26fbba881099
John Michael <jm@byjohnmichael.com>
2026-04-14 01:47:50 -0600

    Site reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { NavigationProvider } from './contexts/NavigationContext';
-import { PageLoader } from './components/PageLoader';
 import Projects from './pages/Projects';
 import Donate from './pages/Donate';
 import Home from './pages/Home';
 import React from 'react';
-import './App.css';
 
 function App(): React.ReactElement {
     return (
         <Router>
-            <NavigationProvider>
-                <PageLoader>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/projects" element={<Projects />} />
-                        <Route path="/donate" element={<Donate />} />
-                    </Routes>
-                </PageLoader>
-            </NavigationProvider>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/donate" element={<Donate />} />
+            </Routes>
         </Router>
     );
 } export default App; // By John Michael
